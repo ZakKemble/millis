ac3a8f5023a22d71467181d7b25810b12753bae6
Juriy Petrochenkov <jupetr@gmail.com>
2022-05-23 17:27:19 +0300

    On master: Fix: Inaccuracy in calculation of the Output Compare Register value

--- millis/millis.c
+++ millis/millis.c
@@ -126,7 +126,7 @@ void millis_init()
 	SET_TCCRA();
 	SET_TCCRB();
 	REG_TIMSK = _BV(BIT_OCIE);
-	REG_OCR = ((F_CPU / PRESCALER) / 1000);
+	REG_OCR = ((F_CPU / PRESCALER) / 1000 - 1);
 }
 
 // Get current milliseconds
